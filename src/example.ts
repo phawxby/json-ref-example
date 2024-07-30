232b889126c589624578c9cea4eb94251b2621b2
Paul Hawxby <paul.hawxby@myenergi.com>
2024-07-30 22:26:36 +0100

    extend example

--- src/example.ts
+++ src/example.ts
@@ -1,9 +1,17 @@
 import RefParser from "@apidevtools/json-schema-ref-parser";
 import { join } from "path";
+import { readFile } from "fs/promises";
+import Ajv from "ajv/dist/2019";
 
 (async () => {
   const fullPath = join(__dirname, "schema/root.schema.json");
   const bundled = await RefParser.bundle(fullPath);
 
   console.dir(bundled, { depth: 10 });
+
+  const ajv = new Ajv({ strict: false });
+  ajv.validate(
+    bundled,
+    await readFile(join(__dirname, "example.json"), "utf-8")
+  );
 })();
